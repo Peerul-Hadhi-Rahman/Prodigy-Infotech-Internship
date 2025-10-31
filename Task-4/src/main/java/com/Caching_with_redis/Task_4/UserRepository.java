4c368ec5d4782a1e4f2407f1f10eb8b2c0bb0ec2
Peerul-Hadhi-Rahman <peerulrahman.sde@gmail.com>
2025-11-01 01:40:55 +0530

    Removed Task-4

--- Task-4/src/main/java/com/Caching_with_redis/Task_4/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Caching_with_redis.Task_4;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
