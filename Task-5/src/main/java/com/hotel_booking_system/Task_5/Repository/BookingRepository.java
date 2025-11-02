bc555c8fb3db54fb74e5b38624c0d844f37712f1
Peerul-Hadhi-Rahman <peerulrahman.sde@gmail.com>
2025-11-02 18:40:53 +0530

    Removed Task-5

--- Task-5/src/main/java/com/hotel_booking_system/Task_5/Repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel_booking_system.Task_5.Repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hotel_booking_system.Task_5.model.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-	List<Booking> findByUserId(Long userId);
-}
--- Task-5/src/main/java/com/hotel_booking_system/Task_5/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.hotel_booking_system.Task_5.model;
-
-public enum Role {
-	USER,
-	ADMIN
-}
